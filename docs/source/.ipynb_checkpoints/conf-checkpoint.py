29965605228ce33c05e59cedf18e9a7f70838bc4
Brandon Rodriguez <brandonrodriguez@macbookpro.lan>
2024-11-24 13:52:36 -0500

    Deploying

---
+++ docs/source/.ipynb_checkpoints/conf-checkpoint.py
@@ -0,0 +1,22 @@
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'Quarto Project'
+copyright = '2024'
+author = 'User'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx_book_theme'
+]
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/your-username/your-repo",
+    "use_repository_button": True,
+}
+html_static_path = ['_static']
